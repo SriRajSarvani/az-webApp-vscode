206cfc718ff663c2915b2b867b6ce1933ba99b86
RajaniSrikanth-github <sreekanthdotnet@hotmail.com>
2026-01-20 00:56:06 +0530

    Added EFCore and database connections

---
+++ az-webApp-vscode-Srikanth/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace az_webApp_vscode_Srikanth.Data
+{
+    public  class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+
+       
+    }
+}
+
--- az-webApp-vscode-Srikanth/Program.cs
+++ az-webApp-vscode-Srikanth/Program.cs
@@ -1,14 +1,23 @@
-using Azure.Identity;
-var builder = WebApplication.CreateBuilder(args);
+using az_webApp_vscode_Srikanth.Data;
+//using Azure.Identity;
+using Microsoft.EntityFrameworkCore;
 
+
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("AzureSqlConnection");
+//var connectionString = builder.Configuration.GetConnectionString("Server=tcp: az-webapp-serv.database.windows.net,1433;Initial Catalog=az-webapp-database;Persist,Security Info=False;User,ID=srikanth;Password=P@ssword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+
 builder.Services.AddRazorPages();
 builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
 {
     ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
 });
-
+    
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
